135edae91ec55d40a2b1a674bafb3e5e62f8dfed
Maxim <maxim.baskakov@bk.ru>
2024-07-17 15:43:07 +0300

    fix connect

--- connect.php
+++ connect.php
@@ -4,4 +4,4 @@
     define('USERNAME', 'cr46629_techart');
     define('PASSWORD', 'ZVZ2NyCO');
 
-    $pdo = new PDO("mysql:host=HOST;dbname=DATABASE_NAME;", USERNAME, PASSWORD);
+    $pdo = new PDO("mysql:host=" . HOST .";dbname=" . DATABASE_NAME . ";", USERNAME, PASSWORD);
